a59d823370bf0b6194937fcbfe82e0c68304af47
Chris VanDoo <cvandood@gmail.com>
2026-06-05 10:17:00 -0400

    add template

---
+++ leetcode/template.py
@@ -0,0 +1,28 @@
+"""
+
+"""
+
+TESTS = [
+    {
+        "input": "",
+        "expected": "",
+    },
+    {
+        "input": "",
+        "expected": "",
+    }
+]
+
+def solution(input):
+    return input
+
+def test():
+    for test in TESTS:
+        result = solution(test["input"])
+        if result == test.get("expected"):
+            print ("SUCESS! {} == {}".format(result, test.get("expected")))
+        else:
+            print("FAIL. {} != {}".format(result, test.get("expected")))
+
+if __name__ == "__main__":
+    test()
